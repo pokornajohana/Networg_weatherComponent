d2e84effaf88ee9420a96e4a7fdeab6a41095206
Johana Pokorná <pokorna.johana1@gmail.com>
2023-09-22 14:48:30 +0200

    connect API with real data

--- FirstWeatherComponent/src/components/WeatherCard.tsx
+++ FirstWeatherComponent/src/components/WeatherCard.tsx
@@ -1,6 +1,7 @@
 // import React from 'react';
 // import * as ReactDOM from 'react-dom';
 import * as React from 'react';
+import { useState } from 'react';
 import './index.css';
 import { IWeatherData } from './App';
 import {
@@ -9,20 +10,16 @@ import {
   SunnyIcon,
   SnowIcon,
   RainSnowIcon,
+  RainIcon,
+  ThunderstormsIcon,
+  FogIcon,
 } from '@fluentui/react-icons-mdl2';
 
 export interface WeatherComponentProps {
   weatherData: IWeatherData[];
 }
-const Fetch = fetch(
-  'https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=3811b08e86d8a57f3e32a31eb5fc27b6',
-)
-  .then((resp) => resp.json())
-  .then((data) => console.log(data));
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=3811b08e86d8a57f3e32a31eb5fc27b6
-export const WeatherCard: React.FC<WeatherComponentProps> = ({
-  weatherData,
-}) => {
+
+export const WeatherCard = () => {
   const daysOfWeek = [
     'Sunday',
     'Monday',
@@ -32,55 +29,82 @@ export const WeatherCard: React.FC<WeatherComponentProps> = ({
     'Friday',
     'Saturday',
   ];
+  const [day, setDay] = useState('');
+  const [weather, setWeather] = useState('');
+  const [temperature, setTemperature] = useState(0);
 
-  const weatherElements = weatherData.map((data, index) => (
-    <div key={index} className="one_day">
-      <p className="name_day">{daysOfWeek[data.date.getDay()]}</p>
-      <p className="date_day">{data.date.toLocaleDateString()}</p>
-      {data.weatherType === 'Cloudy' ? (
+  const Fetch = fetch(
+    // 'https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=3811b08e86d8a57f3e32a31eb5fc27b6',
+    'https://api.openweathermap.org/data/2.5/forecast?lat=50.073658&lon=	14.418540&appid=3811b08e86d8a57f3e32a31eb5fc27b6&units=metric',
+  )
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data.list[0].main.temp);
+      setTemperature(data.list[0].main.temp);
+      console.log(data.list[0].dt_txt);
+      setDay(data.list[0].dt_txt);
+      setWeather(data.list[0].weather[0].main);
+      console.log(data.list[0].weather[0].main);
+    });
+  const weatherElements = (
+    <div className="one_day">
+      <p className="name_day">{day}</p>
+      <p className="date_day">{day}</p>
+
+      {weather === 'Rain' ? (
+        <RainIcon className="weather_icon" />
+      ) : weather === 'Clouds' ? (
         <CloudyIcon className="weather_icon" />
-      ) : data.weatherType === 'Sunny' ? (
-        <SunnyIcon className="weather_icon" />
-      ) : data.weatherType === 'Snow' ? (
+      ) : weather === 'Snow' ? (
         <SnowIcon className="weather_icon" />
-      ) : data.weatherType === 'RainSnow' ? (
-        <RainSnowIcon className="weather_icon" />
+      ) : weather === 'Thunderstorm' ? (
+        <ThunderstormsIcon className="weather_icon" />
+      ) : weather === 'Drizzle' ? (
+        <RainIcon className="weather_icon" />
+      ) : weather === 'Clear' ? (
+        <SunnyIcon className="weather_icon" />
       ) : (
-        <PartlyCloudyDayIcon className="weather_icon" />
+        <FogIcon className="weather_icon" />
       )}
-      <p className="temperature_day">{data.temperature}°C</p>
-      <p className="weather_day">{data.weatherType}</p>
+      <p className="temperature_day">{temperature}°C</p>
+      <p className="weather_day">{weather}</p>
     </div>
-  ));
-
+  );
   return <section className="block">{weatherElements}</section>;
 };
 
-// export const WeatherComponent: React.FC<WeatherComponentProps> = ({
+// export const WeatherCard: React.FC<WeatherComponentProps> = ({
 //   weatherData,
 // }) => {
-//   // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-//   console.log(weatherData);
+//   const daysOfWeek = [
+//     'Sunday',
+//     'Monday',
+//     'Tuesday',
+//     'Wednesday',
+//     'Thursday',
+//     'Friday',
+//     'Saturday',
+//   ]
 
-//   return weatherData.map((data) => (
-//     <div>
-//       <section className="block">
-//         <div className="one_day">
-//           <p className="name_day">{data.date.toLocaleDateString()}</p>
-//           <p className="date_day">{data.date.toLocaleDateString()}</p>
-//           <svg
-//             xmlns="http://www.w3.org/2000/svg"
-//             width="50"
-//             height="50"
-//             viewBox="0 0 24 24"
-//             fill="rgba(0, 0, 0, 1)"
-//           >
-//             <path d="M18.944 11.112C18.507 7.67 15.56 5 12 5 9.244 5 6.85 6.611 5.757 9.15 3.609 9.792 2 11.82 2 14c0 2.757 2.243 5 5 5h11c2.206 0 4-1.794 4-4a4.01 4.01 0 0 0-3.056-3.888zM18 17H7c-1.654 0-3-1.346-3-3 0-1.404 1.199-2.756 2.673-3.015l.581-.102.192-.558C8.149 8.274 9.895 7 12 7c2.757 0 5 2.243 5 5v1h1c1.103 0 2 .897 2 2s-.897 2-2 2z"></path>
-//           </svg>
-//           <p className="temperature_day">{data.temperature}</p>
-//           <p className="weather_day">{data.weatherType}</p>
-//         </div>
-//       </section>
+//   const weatherElements = weatherData.map((data, index) => (
+//     <div key={index} className="one_day">
+//       <p className="name_day">{daysOfWeek[data.date.getDay()]}</p>
+//       <p className="date_day">{data.date.toLocaleDateString()}</p>
+//       {data.weatherType === 'Cloudy' ? (
+//         <CloudyIcon className="weather_icon" />
+//       ) : data.weatherType === 'Sunny' ? (
+//         <SunnyIcon className="weather_icon" />
+//       ) : data.weatherType === 'Snow' ? (
+//         <SnowIcon className="weather_icon" />
+//       ) : data.weatherType === 'RainSnow' ? (
+//         <RainSnowIcon className="weather_icon" />
+//       ) : (
+//         <PartlyCloudyDayIcon className="weather_icon" />
+//       )}
+//       <p className="temperature_day">{data.temperature}°C</p>
+//       <p className="weather_day">{data.weatherType}</p>
 //     </div>
 //   ));
+
+//   return <section className="block">{weatherElements}</section>;
 // };
